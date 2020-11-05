e1effc8b72660339e819995a0400c0c829e8c029
阿貴 <ahkui@outlook.com>
2020-11-05 10:56:31 +0800

    improve code quality

--- src/views/Hall.vue
+++ src/views/Hall.vue
@@ -1,18 +1,14 @@
 <style>
-  .dark-bg {
-    background: #363636;
-  }
+.dark-bg {
+  background: #363636;
+}
 </style>
 
 <template>
   <v-app>
     <v-navigation-drawer app dark color="#1E1E1E">
       <v-sheet class="pa-4" dark>
-        <v-avatar
-          class="mb-4"
-          color="grey darken-1"
-          size="64"
-        ></v-avatar>
+        <v-avatar class="mb-4" color="grey darken-1" size="64"></v-avatar>
         <div class="font-weight-bold">
           {{ account.name }}
         </div>
@@ -27,7 +23,9 @@
             <v-icon class="amber--text" large>mdi-history</v-icon>
           </v-list-item-icon>
           <v-list-item-content>
-            <v-list-item-title class="font-weight-bold text-h6">歷史紀錄</v-list-item-title>
+            <v-list-item-title class="font-weight-bold text-h6"
+              >歷史紀錄</v-list-item-title
+            >
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -37,14 +35,21 @@
         <v-row dark>
           <v-col cols="12">
             <v-card dark>
-              <v-subheader class="white--text font-weight-bold text-h5">當前房間</v-subheader>
+              <v-subheader class="white--text font-weight-bold text-h5"
+                >當前房間</v-subheader
+              >
               <v-list two-line>
                 <template v-for="(round, roundIdx) in rounds">
                   <v-list-item :key="roundIdx">
                     <v-list-item-content>
                       <v-list-item-title class="text-h4">
                         房間 #{{ round.id }}
-                        <v-btn class="amber font-weight-bold" :href="'/#/room?round=' + round.id" rounded>進入房間</v-btn>
+                        <v-btn
+                          class="amber font-weight-bold"
+                          :href="'/#/room?round=' + round.id"
+                          rounded
+                          >進入房間</v-btn
+                        >
                       </v-list-item-title>
                       <v-simple-table dark>
                         <thead>
@@ -65,17 +70,24 @@
                         <tbody>
                           <tr>
                             <td>
-                              已被購買<v-icon class="red--text">mdi-close</v-icon>
-                              /
-                              可購買<v-icon class="teal--text">mdi-check</v-icon>
+                              已被購買<v-icon class="red--text"
+                                >mdi-close</v-icon
+                              >
+                              / 可購買<v-icon class="teal--text"
+                                >mdi-check</v-icon
+                              >
                             </td>
                             <td
                               class="text-center font-weight-bold"
-                              v-for="(item, key) in round.seatData"
+                              v-for="(item, key) in round.seats"
                               :key="key"
                             >
-                              <v-icon class="red--text" v-if="item.status">mdi-close</v-icon>
-                              <v-icon class="teal--text" v-else>mdi-check</v-icon>
+                              <v-icon class="red--text" v-if="item.status"
+                                >mdi-close</v-icon
+                              >
+                              <v-icon class="teal--text" v-else
+                                >mdi-check</v-icon
+                              >
                             </td>
                           </tr>
                         </tbody>
@@ -94,70 +106,97 @@
 </template>
 
 <script>
-  import ScatterJS from 'scatterjs-core';
-  import { JsonRpc } from 'eosjs'
+import ScatterJS from "scatterjs-core";
+import { JsonRpc } from "eosjs";
 
-  export default {
-    name: 'Hall',
+export default {
+  name: "Hall",
 
-    data: () => ({
-      account: '',
-      currency: '',
-      rounds: [],
-      temp:[]
-    }),
-
-    mounted: function() {
-      var network = ScatterJS.Network.fromJson({
-        blockchain: 'eos',
-        protocol: 'https',
-        port: 443,
-        chainId: '8a34ec7df1b8cd06ff4a8abbaa7cc50300823350cadc59ab296cb00d104d2b8f',
-        host: 'eos.ahkui.com',
+  data: () => ({
+    account: "",
+    currency: "",
+    rounds: {},
+    temp: [],
+    network: null,
+    rpc: null,
+    isConnected: false,
+    isLogin: false,
+    liveRoundList: [],
+  }),
+  methods: {
+    async fetchLiveRoom() {
+      const data = await this.rpc.get_table_rows({
+        json: true,
+        code: "stb",
+        scope: "stb",
+        table: "liverounds",
+        limit: 5,
       });
-      var rpc = new JsonRpc(network.fullhost());
-      ScatterJS.connect('ahkui-steve', {network}).then(connected => {
-
-        if(!connected) console.error('Could not connect to Scatter.');
-
-        ScatterJS.login().then(id => {
-
-          if(!id) console.error('Could not find any ID.');
-
-          this.account = ScatterJS.account('eos');
-
-          setInterval(async () => {
-
-            rpc.get_currency_balance('eosio.token', this.account.name, 'EOS').then(currency => {
-              this.currency = currency[0];
-            });
-
-            rpc.get_table_rows({
-              json: true,
-              code: 'stb',
-              scope: 'stb',
-              table: 'liverounds',
-              limit: 5,
-            }).then(data => {
-              data['rows'].forEach(round => {
-                rpc.get_table_rows({
-                  json: true,
-                  code: 'stb',
-                  scope: 'stb',
-                  table: 'rounds',
-                  lower_bound: round.id,
-                  limit: 1
-                }).then(seat => {
-                  round['seatData'] = seat['rows'][0]['seats'];
-                  this.temp.push(round);
-                });
-              });
-              this.rounds = this.temp.slice();
-              this.temp = [];
-            });
-          }, 2000);
-        });
+      this.liveRoundList = data.rows.map((x) => x.id);
+    },
+    async fetchRoom() {
+      this.liveRoundList.forEach((id) => {
+        this.rpc
+          .get_table_rows({
+            json: true,
+            code: "stb",
+            scope: "stb",
+            table: "rounds",
+            lower_bound: id,
+            limit: 1,
+          })
+          .then((data) => {
+            const round = data.rows[0];
+            this.rounds[round.id] = round;
+            this.rounds = Object.assign({}, this.rounds);
+          });
       });
+    },
+    async connect() {
+      this.isConnected = await ScatterJS.connect("ahkui-steve", {
+        network: this.network,
+      });
+      if (!this.isConnected) console.error("Could not connect to Scatter.");
+    },
+    async login() {
+      if (this.isConnected) {
+        this.isLogin = await ScatterJS.login();
+        if (this.isLogin) {
+          this.account = ScatterJS.account("eos");
+          this.balanceInterval = setInterval(this.getBalance, 1000);
+        }
+      }
+    },
+    async getBalance() {
+      this.currency = (
+        await this.rpc.get_currency_balance(
+          "eosio.token",
+          this.account.name,
+          "EOS"
+        )
+      )[0];
+    },
+  },
+
+  mounted: async function () {
+    this.network = ScatterJS.Network.fromJson({
+      blockchain: "eos",
+      protocol: "https",
+      port: 443,
+      chainId:
+        "8a34ec7df1b8cd06ff4a8abbaa7cc50300823350cadc59ab296cb00d104d2b8f",
+      host: "eos.ahkui.com",
+    });
+    this.rpc = new JsonRpc(this.network.fullhost());
+
+    try {
+      await this.connect();
+      setInterval(this.fetchLiveRoom, 1000);
+      setInterval(this.fetchRoom, 1000);
+      await this.login();
+    } catch (error) {
+      console.error(error);
     }
-  }
+  },
+};
 </script>
